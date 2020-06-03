578b7b3f7f626e64cb66ace0dbbf9e2257b58cea
Jake Weinstein <jake@aospa.co>
2020-06-02 20:56:08 -0400

    sm8250-common: Add init script to set properties per variant.
     * Adapted from LineageOS.
    
    Change-Id: Ief5d98e41a5808d057096b25a6e743e608660e12

--- common.mk
+++ common.mk
@@ -308,6 +308,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
+# Init
+PRODUCT_PACKAGES += \
+    libinit_sm8250
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml \
