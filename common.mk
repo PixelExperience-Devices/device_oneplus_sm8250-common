07b12bff8f1ebd2657692f02dec773e6c4c1a45e
TARKZiM <tom8476oo@gmail.com>
2020-10-09 00:33:52 +0800

    sm8250-common: Build Health HAL 2.0
    Co-authored-by: HELLBOY017 <abhaygill017@gmail.com>

--- common.mk
+++ common.mk
@@ -293,8 +293,7 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
+    android.hardware.health@2.0-service
 
 # HIDL
 PRODUCT_PACKAGES += \
