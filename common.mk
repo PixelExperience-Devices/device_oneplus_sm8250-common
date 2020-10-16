5d3b75c340a3a114192df5b228e2d2d97ae8f840
chandra <chandra.chaganti@hotmail.com>
2020-10-16 10:28:31 +0200

    sm8150-common: added oneplus camera

--- common.mk
+++ common.mk
@@ -23,6 +23,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/oneplus/sm8250-common/sm8250-common-vendor.mk)
 
+# OP Camera
+$(call inherit-product, device/oneplus/opcamera/opcamera-vendor.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
@@ -97,10 +100,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
-
-# Camera
-PRODUCT_PACKAGES += \
-    Snap
 
 # Common init scripts
 PRODUCT_PACKAGES += \
