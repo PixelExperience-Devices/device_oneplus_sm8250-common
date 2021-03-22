faaf2dd05d33bb9f440234b513ffbd6b3c8fc059
Andrew Hexen <SyberHexen@gmail.com>
2021-03-22 16:09:11 +0100

    sm8250-common: power: Switch to redfin power init script
    * Also drop the qcom.post_boot.sh
    
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>

--- common.mk
+++ common.mk
@@ -206,7 +206,7 @@ PRODUCT_PACKAGES += \
     init.qcom.early_boot.sh \
     init.qcom.efs.sync.sh \
     init.qcom.factory.rc \
-    init.qcom.post_boot.sh \
+    init.qcom.power.rc \
     init.qcom.rc \
     init.qcom.sdio.sh \
     init.qcom.sh \
