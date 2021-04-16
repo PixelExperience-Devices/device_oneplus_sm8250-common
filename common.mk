d343d6070fd43530899336a3a4df47f07ee20140
Chandra Chaganti <chandrachaganti@pixelexperience.org>
2021-04-16 06:32:17 +0200

    sm8250-common: Nuke LiveDisplay
    Change-Id: I98d72b3f75afecffee37f40b58dfffb745f18179

--- common.mk
+++ common.mk
@@ -246,10 +246,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.1-service.oplus
-
 # Media
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
