24c00988208e4c78403a48f97eb2406ed3acc7ae
Rohan Hasabe <rohanhasabe8@gmail.com>
2021-10-14 12:39:51 +0200

    sm8250-common: Build vendor HIDL interfaces
    Change-Id: I8695467802fdaccef3887d1cfc76a6313feaef83
    Signed-off-by: Rohan Hasabe <rohanhasabe8@gmail.com>

--- common.mk
+++ common.mk
@@ -23,6 +23,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/oneplus/sm8250-common/sm8250-common-vendor.mk)
 
+# Include new Interfaces makefile
+$(call inherit-product, $(LOCAL_PATH)/interfaces.mk)
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
