1c538024d203f1005a192bf8af2672d5280f6ce3
Rohan Hasabe <rohanhasabe8@gmail.com>
2021-10-18 17:47:27 +0200

    [REVERTME] sm8250-common: Drop bootjars

--- common.mk
+++ common.mk
@@ -371,11 +371,11 @@ PRODUCT_PACKAGES += \
     libstagefrighthw
 
 # OnePlus
-PRODUCT_PACKAGES += \
-    oneplus-fwk
-
-PRODUCT_BOOT_JARS += \
-    oneplus-fwk
+#PRODUCT_PACKAGES += \
+#    oneplus-fwk
+#
+#PRODUCT_BOOT_JARS += \
+#    oneplus-fwk
 
 # Power
 PRODUCT_PACKAGES += \
@@ -416,8 +416,8 @@ PRODUCT_PACKAGES += \
     qti_telephony_utils.xml \
     telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
+#PRODUCT_BOOT_JARS += \
+#    telephony-ext
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
@@ -496,8 +496,8 @@ PRODUCT_PACKAGES += \
     libnl \
     libwfdaac_vendor
 
-PRODUCT_BOOT_JARS += \
-    WfdCommon
+#PRODUCT_BOOT_JARS += \
+#    WfdCommon
 
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
