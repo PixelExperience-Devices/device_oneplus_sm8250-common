98e0ff7aac6ca6f3ca7cc981fb85443b2792fc28
TheScarastic <warabhishek@gmail.com>
2021-11-01 13:15:34 +0530

    sm8250-common: Switch to udfps

--- common.mk
+++ common.mk
@@ -277,8 +277,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.oneplus \
-    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.oneplus
+    android.hardware.biometrics.fingerprint@2.3-service.oneplus \
+    vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 
 # GPS
 PRODUCT_COPY_FILES += \
